ef454f81304c3238beb69f4b08b080b0dcd8fc6f
MaxiCamilo@github.com <maxi_camilo@yahoo.com>
2024-08-30 17:29:21 -0300

    Add prefix

--- lib/src/prefixs/petition_data_type.dart
+++ lib/src/prefixs/petition_data_type.dart
@@ -1,5 +1,7 @@
-const String contentTypeJson = 'application/json; charset=utf-8';
-const String contentTypeText = 'text/plain; charset=utf-8';
-const String contentTypeXml = 'application/xml; charset=utf-8';
-const String contentTypeBinary = 'application/octet-stream';
-const String contentTypeZip = 'application/zip';
+mixin ContentType {
+  static const String contentTypeJson = 'application/json; charset=utf-8';
+  static const String contentTypeText = 'text/plain; charset=utf-8';
+  static const String contentTypeXml = 'application/xml; charset=utf-8';
+  static const String contentTypeBinary = 'application/octet-stream';
+  static const String contentTypeZip = 'application/zip';
+}
