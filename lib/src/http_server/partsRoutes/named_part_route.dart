ff0681f85498d0f99ea9324e3e5df436c7041412
Maximiliano Camilo <MaxiCamilo@github.com>
2025-04-14 17:36:35 -0300

    The language system and file operators have changed

--- lib/src/http_server/partsRoutes/named_part_route.dart
+++ lib/src/http_server/partsRoutes/named_part_route.dart
@@ -16,22 +16,22 @@ class NamedPartRoute with IPartRoute {
   void addValue({required String part, required Map<String, dynamic> namedValues}) {
     switch (type) {
       case PrimitiesType.isInt:
-        namedValues[name] = GeneralConverter(part).toInt(propertyName: TranslatedText(message: name));
+        namedValues[name] = GeneralConverter(part).toInt(propertyName: TranslatedOration(message: name));
         break;
       case PrimitiesType.isDouble:
-        namedValues[name] = GeneralConverter(part).toDouble(propertyName: TranslatedText(message: name));
+        namedValues[name] = GeneralConverter(part).toDouble(propertyName: TranslatedOration(message: name));
         break;
       case PrimitiesType.isNum:
-        namedValues[name] = GeneralConverter(part).toDouble(propertyName: TranslatedText(message: name));
+        namedValues[name] = GeneralConverter(part).toDouble(propertyName: TranslatedOration(message: name));
         break;
       case PrimitiesType.isString:
         namedValues[name] = part.toString();
         break;
       case PrimitiesType.isBoolean:
-        namedValues[name] = GeneralConverter(part).toBoolean(propertyName: TranslatedText(message: name));
+        namedValues[name] = GeneralConverter(part).toBoolean(propertyName: TranslatedOration(message: name));
         break;
       case PrimitiesType.isDateTime:
-        namedValues[name] = GeneralConverter(part).toDateTime(propertyName: TranslatedText(message: name));
+        namedValues[name] = GeneralConverter(part).toDateTime(propertyName: TranslatedOration(message: name));
         break;
       case PrimitiesType.isBinary:
         throw NegativeResult(identifier: NegativeResultCodes.implementationFailure, message: Oration(message: '¡Binary interpretation was not created!'));
